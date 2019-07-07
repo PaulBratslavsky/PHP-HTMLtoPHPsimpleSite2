864c38e1826b2450e4ccebd8be221700afe63822
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2019-07-07 01:28:29 -0400

    quick edit

--- suggest.php
+++ suggest.php
@@ -10,7 +10,7 @@
   <h2><span>Show Suggested<span></h2>
 
   <div class="show-random">
-    <?php $random = array_rand($catalog, 4); ?>
+    <?php $random = array_rand($catalog, 6); ?>
     <?php foreach ( $random as $id ) : ?>
       <?php echo showSingleItemFunc($id, $catalog[$id]); ?> 
     <?php endforeach; ?>
